81c7fd2c326aa4cb76af0ebf52af7214c5fca237
jaferidrees <jaferll@gmail.com>
2023-02-10 08:56:32 +0100

    style book record

--- src/components/BookRecord.js
+++ src/components/BookRecord.js
@@ -1,5 +1,5 @@
 import React from "react";
-import ListGroup from 'react-bootstrap/ListGroup';
+import Image from 'react-bootstrap/Image'
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
@@ -8,28 +8,26 @@ import universal_cover from '../assets/images/universal_Book_cover.png'
 const BookRecord = (props) => {
     console.log('prp', props)
     const { title, author, first_sentence, lang, publisher, cover } = { ...props.book };
-    const coverUrl = (cover? `https://covers.openlibrary.org/b/id/${cover}-M.jpg` : universal_cover);
+    const coverUrl = (cover ? `https://covers.openlibrary.org/b/id/${cover}-M.jpg` : universal_cover);
     return (
         <Container>
             {/* Stack the columns on mobile by making one full-width and the other half-width */}
-            <Row>
-                <Col xs={12} md={4}>
-                    <img src={coverUrl} alt="Cover" />
+            <Row className="border rounded gap-3 m-3">
+                <Col xs={12} md={6}>
+                    <p className="display-6 text-center">{title}</p>
+                    <div class="text-center">
+                    <Image src={coverUrl} fluid={true} alt="Cover" className="shadow"/>
+                    </div>
                 </Col>
-                <Col xs={6} md={2}>
-                    {title}
-                </Col>
-                <Col xs={6} md={2}>
-                    Author: {author}
+                <Col xs={6} md={4} className="row align-items-center">
+                <p className="display-8">Author: {author}</p>
+                <p className="display-8">Language: {lang}</p>
+                <p className="display-8"> Publisher: {publisher}</p>
+                    
+                   
                 </Col>
                 <Col xs={6} md={2}>
                     First sentence: {first_sentence}
-                </Col>
-                <Col xs={6} md={2}>
-                    Language: {lang}
-                </Col>
-                <Col xs={6} md={2}>
-                    Publisher: {publisher}
                 </Col>
             </Row>
         </Container>
