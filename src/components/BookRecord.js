2fd72d89fd90961e8b8ee640ba850ff246c17b78
jaferidrees <jaferll@gmail.com>
2023-02-11 17:48:58 +0100

    Add add to my library button

--- src/components/BookRecord.js
+++ src/components/BookRecord.js
@@ -3,33 +3,36 @@ import Image from 'react-bootstrap/Image'
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-import universal_cover from '../assets/images/universal_Book_cover.png'
+import universal_cover from '../assets/images/universal_Book_cover.png';
+import Button from 'react-bootstrap/Button';
 
 const BookRecord = (props) => {
     console.log('prp', props)
     const { title, author, first_sentence, lang, publisher, cover } = { ...props.book };
     const coverUrl = (cover ? `https://covers.openlibrary.org/b/id/${cover}-M.jpg` : universal_cover);
     return (
-        <Container>
+        <Container className="border rounded gap-3 m-3 py-3">
             {/* Stack the columns on mobile by making one full-width and the other half-width */}
-            <Row className="border rounded gap-3 m-3">
+            <Row>
                 <Col xs={12} md={6}>
                     <p className="display-6 text-center">{title}</p>
                     <div class="text-center">
-                    <Image src={coverUrl} fluid={true} alt="Cover" className="shadow"/>
+                        <Image src={coverUrl} fluid={true} alt="Cover" className="shadow" />
                     </div>
                 </Col>
                 <Col xs={6} md={4} className="row align-items-center">
-                <p className="display-8">Author: {author}</p>
-                <p className="display-8">Language: {lang}</p>
-                <p className="display-8"> Publisher: {publisher}</p>
-                    
-                   
+                    <p className="display-8">Author: {author}</p>
+                    <p className="display-8">Language: {lang}</p>
+                    <p className="display-8"> Publisher: {publisher}</p>
                 </Col>
-                <Col xs={6} md={2}>
-                    First sentence: {first_sentence}
+                <Col className="row align-items-center pe-3">
+                <Button variant="primary">Add to my library</Button>
                 </Col>
             </Row>
+            {first_sentence &&
+                <Row className="mt-5">
+                    <p>First sentence: {first_sentence}</p>
+                </Row>}
         </Container>
     )
 }
