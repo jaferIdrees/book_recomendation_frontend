d5be6c11c8480a63db5b571ea0c4cc520dd2bd21
jaferidrees <jaferll@gmail.com>
2023-02-14 06:55:57 +0100

    Add add_bookJS to add a book from open lib search to our api

---
+++ src/API/add_book.js
@@ -0,0 +1,17 @@
+import { user_logged } from "./user_authentication"
+
+const add_book = async (book) => {
+   // await user_logged().then((res)=>console.log(res))
+    if (user_logged()) {
+        fetch("http://localhost:4000/api/v1/books", {
+            method: "post",
+            headers: {
+                "Content-Type": "application/json",
+                Authorization: localStorage.getItem("token"),
+            },
+            body: JSON.stringify({ book }),
+        })
+    }
+}
+
+export default add_book
