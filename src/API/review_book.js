b11515e6649031cc5598ba3d984ba50466f256ba
jaferidrees <jaferll@gmail.com>
2023-02-16 16:30:55 +0100

    Update reveiw_book with the fetch statement

--- src/API/review_book.js
+++ src/API/review_book.js
@@ -1,5 +1,13 @@
-const review_book_api = (isbn) => {
-    console.log('review', isbn)
+const review_book_api = (review) => {
+    console.log('review', review.isbn)
+    fetch("http://localhost:4000/api/v1/books/7/reviews", {
+        method: "post",
+        headers: {
+            "Content-Type": "application/json",
+            Authorization: localStorage.getItem("token"),
+        },
+        body: JSON.stringify(review),
+    })
 }
 
 export default review_book_api;
