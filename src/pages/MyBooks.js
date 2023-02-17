286debd07339cba82025a0a221c64474e90ea95b
jaferidrees <jaferll@gmail.com>
2023-02-17 18:40:48 +0100

    Update MyBooks page

--- src/pages/MyBooks.js
+++ src/pages/MyBooks.js
@@ -1,22 +1,27 @@
 import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom"
+import { Link, useParams } from "react-router-dom"
 import retrieveUserBooks from "../API/user_books";
 import BookRecord from "../components/BookRecord";
 
 const MyBooks = () => {
     const [bookList, setBookList] = useState([]);
-    const {id} = useParams();
+    const { id } = useParams();
     console.log(id);
-    useEffect(()=>{
+    useEffect(() => {
         const books = async (book_id) => retrieveUserBooks(book_id)
-        books(id).then((list)=> setBookList(list))
-    },[id])
-    if (bookList){
-    return (
-        <>
-        {bookList.map((book) => <BookRecord key={book.isbn} book={book} btn="review"/>)}
-        </>
-    );} else return null;
+        books(id).then((list) => setBookList(list))
+    }, [id])
+    if (bookList) {
+        return (
+            <>
+                {bookList.map((book) => <div>
+                    <Link to={`/books/${book.id}`} key={book.id}>
+                        <BookRecord book={book} btn="review" />
+                    </Link>
+                </div>)}
+            </>
+        );
+    } else return null;
 }
 
 export default MyBooks;
