202ffb39dbcbca352ddbb29c2bb5393e1ed250aa
jaferidrees <jaferll@gmail.com>
2023-02-17 18:46:00 +0100

    Add ReviewRecord component

---
+++ src/components/ReviewRecord.js
@@ -0,0 +1,16 @@
+import { Rating } from "react-simple-star-rating";
+
+const ReviewRecord = (props) => {
+    const {content, rating, full_name} = props.review;
+
+    return (
+        <div className="d-flex justify-content-between border rounded p-3 mx-3">
+            <p>Review: {content}</p>
+            
+            <p>Rating: <Rating readonly="true" initialValue={rating} /></p>
+            <p>By: {full_name}</p>
+        </div>
+    )
+}
+
+export default ReviewRecord;
