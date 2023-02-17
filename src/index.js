e5663731ae71564de840a19eb0aad3427bce8a22
jaferidrees <jaferll@gmail.com>
2023-02-17 18:37:00 +0100

    update indexJS adding the navbar component

--- src/index.js
+++ src/index.js
@@ -3,10 +3,12 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import MainMenu from "./components/MainMenu";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
 	<React.StrictMode>
+		<MainMenu/>
 		<App />
 	</React.StrictMode>,
 );
